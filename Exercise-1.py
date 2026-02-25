2e45e78954690dc149a0db4a9d0d7695110930b6
Jarovie Salaum <salaum.jarovie@gmail.com>
2026-02-25 08:03:36 +0800

    Exercise-3

--- Exercise-1.py
+++ Exercise-1.py
@@ -2,14 +2,7 @@
 
 
 
-# positive, negative, or zero
-#fromUser = int(input("Enter number: "))
-#if fromUser > 0:
-#    print("Number is positive.")
-#elif fromUser < 0:
-#    print("Number is negative.")
-#else:
-#    print("Number is zero.")
+
 
 # voting eligibility
 #fromUser = int(input("Enter age: "))
