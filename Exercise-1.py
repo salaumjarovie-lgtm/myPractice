06c6cc14f7223046a01dc60482230d73fcb574d1
Jarovie Salaum <salaum.jarovie@gmail.com>
2026-02-25 08:10:31 +0800

    Exercise-1

--- Exercise-1.py
+++ Exercise-1.py
@@ -1,11 +1,1 @@
-#print("Hello! This is my first Python program.")
-
-
-
-# print even numbers
-count = 0
-for i in range(20):
-    for i %2 == 0:
-        count += 1
-    
-    print(i)
+print("Hello! This is my first Python program.")
---
+++ Exercise-7.py
@@ -0,0 +1,7 @@
+# print even numbers
+count = 0
+for i in range(20):
+    for i %2 == 0:
+        count += 1
+    
+    print(i)
